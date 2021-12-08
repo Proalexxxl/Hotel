0da64ed76f4c9077a309aaef0a64af5766deb049
Alex <Proalexxxl@gmail.com>
2021-12-08 17:45:56 +0200

    refactor constantsDB

--- src/base/ConstantsDB.java
+++ src/base/ConstantsDB.java
@@ -3,7 +3,6 @@ package base;
 public class ConstantsDB {
 
     public final String DB_DRIVER = "jdbc:sqlite:";
-//    public final String DB_DRIVER = "jdbc:sqlite:G:\\\\Hillel\\Hillel_Java_elementary\\";
     public final String DB_BASE_URL = "G:\\\\Hillel\\Hillel_Java_elementary\\projects\\Hotel\\db\\";
     public final String DB_NAME = "HotelDB.db";
 }
